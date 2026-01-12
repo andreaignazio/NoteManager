98e3fb22e0876106c91e812dcc53f6d94f7feca8
Andrea <andreaignazio.russo@gmail.com>
2026-01-12 09:25:31 +0100

    Config-2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
+const domain = import.meta.env.VITE_API_URL || ''; 
+
 const api = axios.create({
-  //baseURL: '/api',
-  //headers: {'Content-Type': 'application/json'}
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  
+  baseURL: `${domain}/api`, 
   headers: {
     'Content-Type': 'application/json',
-  }
+  },
 });
 
 //Add token to requests
