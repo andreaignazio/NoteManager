0e312f1c83be624d8e4244cd04b155808d1d73ce
Andrea <andreaignazio.russo@gmail.com>
2026-01-12 09:18:55 +0100

    Database congig

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  //baseURL: '/api',
   //headers: {'Content-Type': 'application/json'}
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 //Add token to requests
